379cd2acc2b83d8fbaede69d8543add11366a411
DaryabiTB <talibdaryabi@gmail.com>
2024-01-25 15:39:47 +0100

    edited and run_test.py to be able to handle the command line execution

--- run_tests.py
+++ run_tests.py
@@ -2,10 +2,19 @@ import json
 import logging
 import random
 import importlib
+import argparse
+import sys
 
 # Initialize logging
 logging.basicConfig(filename='logs/project_log.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Run emission tracking tools.')
+parser.add_argument('--tool', type=str, help='Specify the tool name or "all" to run all tools.', default='all')
+args = parser.parse_args()
+
+# Load config
 with open("config.json") as config_file:
 	config = json.load(config_file)
 
@@ -18,11 +27,7 @@ def start_all():
 	
 	# Run each test with each tool
 	for tool_name in tools:
-		tool_module = importlib.import_module(f"tools.{tool_name}_tool")
-		for test_case in config["test_cases"]:
-			logging.info(f"Running {test_case} with {tool_name}")
-			tool_module.run_test(test_case, config["tools"][tool_name])
-			logging.info("******")
+		start_tool(tool_name)
 
 
 def start_tool(tool_name):
@@ -35,14 +40,12 @@ def start_tool(tool_name):
 
 
 if __name__ == "__main__":
-	import sys
-	print("sys.path")
-	print(sys.path)
-	print("*" * 100)
-	avalibe_tool = {
-		1: "codecarbon",
-		2: "carbontracker",
-		3: "eco2ai",
-		4: "energat"
-	}
-	start_tool(avalibe_tool[4])
+	tool_name = args.tool
+	if tool_name == 'all':
+		start_all()
+	else:
+		if tool_name in config["tools"]:
+			start_tool(tool_name)
+		else:
+			print(f"Tool '{tool_name}' not found. Available tools are: {list(config['tools'].keys())}")
+			sys.exit(1)
